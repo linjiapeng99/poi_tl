8c4f11412675311defd8bfbf87f8de580dd14558
linjiapeng <2602902282@qq.com>
2025-12-04 18:23:38 +0800

    poi第二次练习，在测试用例中练习，并且替换$占位符

--- src/main/java/com/example/poi_demo/PoiTest.java
+++ src/main/java/com/example/poi_demo/PoiTest.java
@@ -7,13 +7,13 @@ import com.deepoove.poi.plugin.table.LoopRowTableRenderPolicy;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
 public class PoiTest {
     public static void main(String[] args) throws IOException {
+        //1.数据处理
         List<Person> personList = Arrays.asList(
                 new Person("张三",22,"男","NBKJ","无"),
                 new Person("李四",23,"女","NBKJ","无")
@@ -21,17 +21,19 @@ public class PoiTest {
         // 2. 创建表格渲染策略
         LoopRowTableRenderPolicy policy = new LoopRowTableRenderPolicy();
         // 3. 配置绑定 - 注意：绑定的是模板中的占位符名称
-        Configure config = Configure.builder()
-                .bind("personList", policy).bind("labors", policy).build();
+        ConfigureBuilder builder = Configure.builder();
+        builder.buildGramer("${", "}");
+        Configure configure = builder.build();
+        configure.customPolicy("personList",policy);
         // 4. 指定模板文件路径
-        String templatePath = "D:\\My_work\\poi测试.docx";
-        XWPFTemplate template = XWPFTemplate.compile(templatePath, config).render(
+        String templatePath = "src/main/resources/templateDoc/poi测试.docx";
+        XWPFTemplate template = XWPFTemplate.compile(templatePath ,configure).render(
                 new HashMap<String, Object>() {{
                     put("personList", personList);
                 }}
         );
-        // 6. 输出结果
-        FileOutputStream out = new FileOutputStream("D:\\My_work\\output.docx");
+        // 5. 输出结果
+        FileOutputStream out = new FileOutputStream("src/main/resources/outputDoc/output.docx");
         template.write(out);
         template.close();
         out.close();
--- src/test/java/com/example/poi_demo/PoiDemoApplicationTests.java
+++ src/test/java/com/example/poi_demo/PoiDemoApplicationTests.java
@@ -1,13 +1,46 @@
 package com.example.poi_demo;
 
+import com.deepoove.poi.XWPFTemplate;
+import com.deepoove.poi.config.Configure;
+import com.deepoove.poi.config.ConfigureBuilder;
+import com.deepoove.poi.plugin.table.LoopRowTableRenderPolicy;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
 
 @SpringBootTest
 class PoiDemoApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void testRenderTemplatewith() throws IOException {
+        List<Person> personList = Arrays.asList(
+                new Person("张三", 22, "男", "NBKJ", "无"),
+                new Person("李四", 23, "女", "NBKJ", "无")
+        );
+        ConfigureBuilder builder = Configure.builder();
+        builder.buildGramer("${", "}");
+        String templatePath = "src/main/resources/templateDoc/poi测试.docx";
+        LoopRowTableRenderPolicy policy = new LoopRowTableRenderPolicy();
+        Configure configure = builder.build();
+        configure.customPolicy("personList", policy);
+        XWPFTemplate template = XWPFTemplate.compile(templatePath, configure).render(
+                new HashMap<String, Object>() {{
+                    put("personList", personList);
+                }}
+        );
+        FileOutputStream out = new FileOutputStream("src/main/resources/outputDoc/output.docx");
+        template.write(out);
+        template.close();
+        out.close();
     }
 
 }
